962c5fcc9348cb6bf60fc93f62afb635234fc301
Obi Chinedu Frank <frankieetchy@gmail.com>
2024-08-07 12:52:54 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use axum::{routing::get, Router};
+use routes::health;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+mod routes;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "link_shortener=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    // let app = Router::new().route("/health", get(health));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .expect("Could not initialize TcpListener");
+    Ok(())
 }
