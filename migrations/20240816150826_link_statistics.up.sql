192cb1dc880658876d8e6c6e35efee3af92bb485
Obi Chinedu Frank <frankieetchy@gmail.com>
2024-08-16 16:15:24 +0100

    wip statistics

--- migrations/20240816150826_link_statistics.up.sql
+++ migrations/20240816150826_link_statistics.up.sql
@@ -1,1 +1,12 @@
--- Add up migration script here
+CREATE TABLE IF NOT EXISTS link_statistics
+(
+  id serial PRIMARY KEY,
+  link_id text NOT NULL,
+  referer text,
+  user_agent text,
+  CONSTRAINT fk_links
+  FOREIGN KEY (link_id)
+  REFERENCES links (id)
+);
+
+CREATE INDEX idx_link_statistics_link_id on link_statistics USING btree (link_id);
